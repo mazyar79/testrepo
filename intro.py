477f7b72ac5e66c9b73100aa536dd3a541c728bb
mazyar79 <75303072+mazyar79@users.noreply.github.com>
2024-05-04 14:35:27 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,15 @@
+
+#variables set to strings
+
+greeting = 'Hello'
+name = 'Michael'
+
+#string formatting
+#message = '{}, {}. Welcome!'.format(greeting, name)
+
+#fstring formatting with using upper method to change name to all uppercase
+message = f'{greeting}, {name.upper()}. Welcome!'
+
+# Print Welcome Message
+print(message)
+
